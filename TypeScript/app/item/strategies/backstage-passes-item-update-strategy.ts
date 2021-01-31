ba35ec07672185208fd5beceeef0ad38eace260b
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-02-01 00:00:32 +0100

    feat: implement backstage passes item update strategy

--- TypeScript/app/item/strategies/backstage-passes-item-update-strategy.ts
+++ TypeScript/app/item/strategies/backstage-passes-item-update-strategy.ts
@@ -3,6 +3,12 @@ import { Item } from "../../gilded-rose";
 
 export class BackstagePassesItemUpdateStrategy extends ItemUpdateStrategy {
     updateQuality(item: Item): Item {
+        if (item.sellIn <= 0) item.quality = 0;
+        else if (item.sellIn <= 5) item.quality = Math.min(item.quality + 3, this.MAX_QUALITY);
+        else if (item.sellIn <= 10) item.quality = Math.min(item.quality + 2, this.MAX_QUALITY);
+        else item.quality = Math.min(item.quality + 1, this.MAX_QUALITY);
+
+        item.sellIn -= 1;
         return item;
     }
 }
