d8940c8d2ec1c7b82eacbf4ec2fdce8fa8b737df
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-01-31 18:18:33 +0100

    test: add coverage for Aged Brie item

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -9,4 +9,31 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('foo');
     });
 
+    describe('Aged Brie', () => {
+        it('should decrease the sellIn of the object', () => {
+            const gildedRose = new GildedRose([new Item('Aged Brie', 0, 2)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].sellIn).to.equal(-1);
+        });
+
+        it('should increase the quality after update', () => {
+            const gildedRose = new GildedRose([new Item('Aged Brie', 20, 2)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(3);
+        });
+
+        it('should increase the quality two times faster if the sell by date has passed', () => {
+            const gildedRose = new GildedRose([new Item('Aged Brie', 0, 2)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(4);
+        });
+
+        it('should never increase the quality over 50', () => {
+            const gildedRose = new GildedRose([new Item('Aged Brie', 20, 50)]);
+            const items = gildedRose.updateQuality();
+            expect(items[0].quality).to.equal(50);
+        });
+
+    });
+
 });
